8590d0d13b4d57209593164e608e046aa5ce02ff
Maria Gutierrez Martin <maria_ventas98@hotmail.es>
2021-11-06 18:09:03 +0100

    Private method done but not finished yet.

--- src/main/java/com/upm/prof/a/services/ConsultationService.java
+++ src/main/java/com/upm/prof/a/services/ConsultationService.java
@@ -1,6 +1,11 @@
 package com.upm.prof.a.services;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.List;
+
+import com.google.gson.Gson;
 
 import com.upm.prof.a.persistence.Consultation;
 import com.upm.prof.a.persistence.User;
@@ -12,6 +17,9 @@ import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+
 
 @Service
 public class ConsultationService {
@@ -22,20 +30,37 @@ public class ConsultationService {
     @Autowired
     private UserRepo userRepo;
 
+    private static final Gson gson = new Gson();
+
     public List<Consultation> getConsultation(Long id) throws NotFoundException {
         User currentUser = userRepo.findById(id).orElseThrow(() -> 
             new NotFoundException());
 		List<Consultation> consultations = consultationRepo.findByUserId(id);
-                 
+		for (Consultation consultation: consultations){
+            consultation.setUpdateTime(this.getLastUpdate(consultation));
+        }
 		return consultations;
 	}
 
 	public Consultation postConsultation (Long id, Consultation consultation) throws NotFoundException {
         User currentUser = userRepo.findById(id).orElseThrow(() ->
                 new NotFoundException());
+        Date date = new Date();
+
         consultation.setUser(currentUser);
+        consultation.setRegistrationTime(date);
+        consultation.setUpdateTime(this.getLastUpdate(consultation));
         consultationRepo.save(consultation);
         return consultation;
     }
-    
+
+    private Date getLastUpdate (Consultation consultation){
+        String url = consultation.getUrl();
+        String llamadaAPI = " http://archive.org/wayback/available?"+ url;
+        RestTemplate llamada = new RestTemplate();
+        ResponseEntity responseEntity = llamada.getForEntity(llamadaAPI,ResponseEntity.class);
+        String resultado = gson.toJson(responseEntity.getBody());
+
+        return null;
+    }
 }
