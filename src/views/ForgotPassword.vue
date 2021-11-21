0c407bb74b0252eaf4a587dfe32953a401b8c5bf
alexfun200 <alexfun200@gmail.com>
2021-11-21 13:46:47 +0100

    LogIn and SignUp

---
+++ src/views/ForgotPassword.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="vue-tempalte">
+        <form>
+            <h3>Forgot Password</h3>
+
+            <div class="form-group">
+                <label>Email address</label>
+                <input type="email" class="form-control form-control-lg" />
+            </div>
+
+            <div class="col text-center">
+            <button type="submit" class="btn btn-primary btn-lg btn-block">Reset password</button>
+            </div>
+
+        </form>
+    </div>
+</template>
+
+<script>
+    // export default 
+    //     data() 
+    //         return 
+        
+    
+</script>
---
+++ src/views/SignUp.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="vue-tempalte">
+        <form>
+            <h3>Sign Up</h3>
+
+            <div class="form-group">
+                <label>Username</label>
+                <input type="text" class="form-control form-control-lg"/>
+            </div>
+
+            <div class="form-group">
+                <label>Email address</label>
+                <input type="email" class="form-control form-control-lg" />
+            </div>
+
+            <div class="form-group">
+                <label>Password</label>
+                <input type="password" class="form-control form-control-lg" />
+            </div>
+            <div class="col text-center">
+                <button type="submit" class="btn btn-primary btn-lg btn-block">Sign Up</button>
+            </div>
+
+            <p class="forgot-password text-right">
+                 
+                <!-- <router-link :to="name 'login'">sign in?</router-link> -->
+                <router-link to="/log/login">Already registered ?</router-link>
+            </p>
+        </form>
+    </div>
+</template>
+
+// <script>
+//     export default 
+//         data() 
+//             return 
+        
+    
+// </script>
